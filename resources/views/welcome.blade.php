b33f3dcf9f6025414770dd9c619a87e0976b7448
bardhansuvajit <bardhansuvajit@gmail.com>
2024-11-27 00:06:59 +0530

    minor updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     </head>
     <body class="">
-        test
+        <?php
+            phpinfo();
+        ?>
     </body>
 </html>
